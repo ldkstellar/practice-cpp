f9292ca407396122e41c076844313ce074598228
leedongkyu <leedong0225@icloud.com>
2024-12-19 15:04:16 +0900

    feat: oneline

--- 7/oneline.cpp
+++ 7/oneline.cpp
@@ -6,7 +6,12 @@ int main(int argc, char const *argv[])
     // 한줄 읽기
 
     string mailHeader;
+    getline(cin, mailHeader, '@'); // '\n' 문자를 만날 때까지 cin에서 문자들을 꺼낸다.
 
+    cout << mailHeader;
+    // 다음 조건을 만족할 때까지 계속해서 스트림에서 문자들을 꺼내 string을 저장
+    // eof을 만날 때 eofbit 값이 true가 된다.
+    // 구분 문자를 만날때까지 (구분 문자는 버려짐)
     /* code */
     return 0;
 }
