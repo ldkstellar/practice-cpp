e0c9a84c3416cbdcfe35050c9b875c1ea2e8dfa3
leedongkyu <leedong0225@icloud.com>
2025-01-25 19:53:28 +0900

    fix: write_binary file

--- 10/write_binary.cpp
+++ 10/write_binary.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-
     ofstream fout("student.dat", ios_base::in | ios_base::binary);
 
     if (fout.is_open())
     {
         const char *buffer = "Pope kim";
-
         fout.write(buffer, strlen(buffer) + 1);
     }
 
@@ -22,7 +21,6 @@ int main(int argc, char const *argv[])
         fin.read(line, sizeof(line));
         cout << line;
     }
-
     fout.close();
     return 0;
 }
