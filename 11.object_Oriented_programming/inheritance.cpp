29e88391d9dfdc67492418b8ce9c44a3c1b5ebc4
leedongkyu <leedongkyu@MacBookAir.local>
2025-03-13 16:05:36 +0900

    feat: inheritance

---
+++ 11.object_Oriented_programming/inheritance.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Animal {
+ public:
+  Animal(int age);
+  friend class Cat;
+
+ private:
+  int mAge;
+};
+
+Animal::Animal(int age) : mAge(age) {}
+
+class Cat : public Animal {
+ public:
+  Cat(int age, const char* name);
+  ~Cat();
+  void print();
+
+ private:
+  char* mName;
+};
+void Cat::print() {
+  std::cout << "이름: " << mName << std::endl;
+  std::cout << "나이" << mAge << std::endl;
+}
+
+Cat::~Cat() {
+  delete[] mName;
+  std::cout << "delete heapMemory" << std::endl;
+}
+
+Cat::Cat(int age, const char* name) : Animal(age) {
+  size_t size = strlen(name) + 1;
+  mName = new char[size];
+  strcpy(mName, name);
+}
+
+int main(int argc, char const* argv[]) {
+  Cat myCat(3, "rusianBlue");
+  myCat.print();
+  /* code */
+  return 0;
+}
