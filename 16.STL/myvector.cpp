ff2efe66f355ec34545213b3a30ae8f476a4fcd8
leedongkyu <leedongkyu0225@gmail.com>
2025-05-05 22:10:22 +0900

    feat: add iterator of vector object

--- 16.STL/myvector.cpp
+++ 16.STL/myvector.cpp
@@ -5,11 +5,17 @@ int main(int argc, char const *argv[]) {
   scores.reserve(2);
   scores.push_back(30);
   scores.push_back(50);
-  std::vector<int> scores2;
-  std::vector<int> scores(scores2);
+
+  std::vector<int> scores2(scores);
 
   scores.pop_back();
+  scores.push_back(29);
   std::cout << "Current capacity : " << scores.capacity() << std::endl;
   std::cout << "Current size : " << scores.size() << std::endl;
+  for (std::vector<int>::iterator iter = scores.begin(); iter != scores.end();
+       ++iter) {
+    std::cout << *iter << std::endl;
+  }
+
   return 0;
 }
