e9d58dd9387ed6f7c355421142612187042feeb4
leedongkyu <leedongkyu0225@gmail.com>
2025-05-22 12:17:10 +0900

    fix: add abstract class method

--- 11.object_Oriented_programming/abstract.cpp
+++ 11.object_Oriented_programming/abstract.cpp
@@ -1,8 +1,22 @@
+#include <iostream>
+
 class Abstract {
  public:
-  virtual ~Abstract();
+  inline virtual void hello() = 0;  // 인라인함수는 최적화
 
- private:
+ protected:
   int mAge;
   /* data */
 };
+class My : public Abstract {
+ public:
+  void hello() {
+    mAge = 13;
+    std::cout << "hello world " << "my age is" << mAge << std::endl;
+  }
+};
+int main(int argc, char const *argv[]) {
+  My my;
+  my.hello();
+  return 0;
+}
