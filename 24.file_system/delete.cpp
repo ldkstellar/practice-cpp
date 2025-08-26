2f4726289c9d5a40c55eb701c90c20c868616747
leedongkyu <leedongkyu0225@gmail.com>
2025-08-26 18:22:54 +0900

    feat: add comment

--- 24.file_system/delete.cpp
+++ 24.file_system/delete.cpp
@@ -3,10 +3,10 @@ namespace fs = std::filesystem;
 
 int main(int argc, char const *argv[]) {
   fs::path currentpath = fs::current_path();
-  fs::remove(currentpath / "sample.txt");
-  fs::remove(currentpath / "car.txt");
-  fs::remove(currentpath / "coppiedfolder");
-  fs::remove(currentpath / "coppiedfolder2");
-  fs::remove(currentpath / "folder/test");
+  fs::remove(currentpath / "copiedfolder");
+  fs::remove(currentpath / "copiedfolder2/test");
+  fs::remove(currentpath /
+             "copiedfolder2");  // 삭제는 재귀적으로 동작하지 않는다.
+  fs::remove(currentpath / "folder");
   return 0;
 }
